0f40f0d1e27e6ecf8eaf089237a496cc6307a402
yuanchengliang <yuanchengliang@zongheng.com>
2021-10-26 02:11:38 +0800

    branch2 commit3

--- src/com/ycl/Branch2.java
+++ src/com/ycl/Branch2.java
@@ -5,7 +5,7 @@ public class Branch2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("branch2...");
-		System.out.println("opt2...");
+		System.out.println("opt3...");
 	}
 
 }
